90309b1481f81e02982e4860ff80d4cd67c545b7
James Jarvis <git@jamesjarvis.io>
2020-04-10 12:38:23 +0100

    :lipstick: Add favicon and title

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import style from "../styles/styles.scss";
 import { NextPage } from "next";
 import React, { useState } from "react";
 import axios from "axios";
-import styles from "../styles/styles.scss";
 import { useHotkeys } from "react-hotkeys-hook";
+import Head from "next/head";
 
 // const helloworld = 'println("Hello, world!")\n';
 const helloworld = `println("Hello, world!")\n// This is a comment\n\nfunc testFunction(a1 int, a2 int) string {\n\treturn a1 + " " + a2\n}\n\ntesting := true\n\nif testing {\n\tarr := []int{5, 3, 9}\n\tfor i := 0; i < 3; i += 1 {\n\t\tprintln(testFunction(i,arr[i]))\n\t}\n} else {\n\tprintln("not testing")\n}`;
@@ -58,17 +58,46 @@ const Home: NextPage<{ userAgent: string }> = () => {
   };
   useHotkeys("command+enter", compile);
 
+  let share = () => {
+    // console.log(btoa(code));
+  };
+
   return (
     <div id={style.main} className={style.dark}>
+      <Head>
+        <title>Risotto 🍲 Play</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+
       <header>
         <div className={style.menu}>
           <span className={style.title}>Risotto 🍲 Play</span>
-          <button
-            className={style.menuButton}
-            onClick={compile}
-          >
+          <button className={style.menuButton} onClick={compile}>
             Run
-            {/* <span className={styles.cmd}>⌘+↵</span> */}
+            {/* <span className={style.cmd}>⌘+↵</span> */}
+          </button>
+          <button className={style.menuButton} onClick={share}>
+            Share
+            {/* <span className={style.cmd}>⌘+S</span> */}
           </button>
         </div>
       </header>
