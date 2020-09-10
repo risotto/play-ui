ed8420cbbf82547693a3a2bf823c5771c924570f
Raphaël Vigée <raphaelvigee@users.noreply.github.com>
2020-09-10 14:11:39 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = withSass({
     };
   },
   env: {
-    playUrl: "https://api.play.risotto.dev",
+    playUrl: "https://api-play.risotto.dev",
     thisUrl: "https://play.risotto.dev"
   },
 });
